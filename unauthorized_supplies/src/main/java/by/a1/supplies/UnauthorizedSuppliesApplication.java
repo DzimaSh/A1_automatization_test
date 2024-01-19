57bf02f24b748de923b4a877c28acb9d24a99141
DzimaSh <dzmitry.shushkevich@gmail.com>
2024-01-19 14:03:58 +0100

    initialize project

---
+++ unauthorized_supplies/src/main/java/by/a1/supplies/UnauthorizedSuppliesApplication.java
@@ -0,0 +1,13 @@
+package by.a1.supplies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnauthorizedSuppliesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnauthorizedSuppliesApplication.class, args);
+    }
+
+}
---
+++ unauthorized_supplies/src/test/java/by/a1/supplies/UnauthorizedSuppliesApplicationTests.java
@@ -0,0 +1,13 @@
+package by.a1.supplies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnauthorizedSuppliesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
