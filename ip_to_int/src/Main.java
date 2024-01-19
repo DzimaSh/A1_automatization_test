91a0484c24a48f14c9924e7ad6ece46380c2d334
DzimaSh <dzmitry.shushkevich@gmail.com>
2024-01-19 10:57:53 +0100

    add help message

--- ip_to_int/src/Main.java
+++ ip_to_int/src/Main.java
@@ -10,6 +10,12 @@ public class Main {
         IpToInt32Converter converter = new IpToInt32Converter();
         for (int i = 0; i < args.length; i++) {
             switch (args[i]) {
+                case "--help":
+                    if (i == 0) {
+                        printHelp();
+                        return;
+                    }
+                    break;
                 case "-32toi":
                     while (i + 1 < args.length && ParseUtils.isNumeric(args[i + 1])) {
                         int ipInt = Integer.parseInt(args[i + 1]);
@@ -28,5 +34,20 @@ public class Main {
                     break;
             }
         }
+    }
+
+    private void printHelp() {
+        System.out.println("""
+        Usage: java Main [options] [arguments]
+
+        Options:
+          --help      Display this help message
+          -32toi      Convert an int32 to an IP address
+          -ito32      Convert an IP address to an int32
+
+        Arguments:
+          After -32toi, provide one or more int32 values
+          After -ito32, provide one or more IP addresses
+        """);
     }
 }
